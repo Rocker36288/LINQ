c81b6dfb6519c5bc8319c9d634d8414582df00cb
仁鈞 林 <tommylin19990127@gmail.com>
2025-10-14 20:00:59 +0800

    1014

---
+++ LinqLabs/HW/FrmHW2.Designer.cs
@@ -0,0 +1,362 @@
+﻿namespace LinqLabs.HW
+{
+    partial class FrmHW2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.lblOrders = new System.Windows.Forms.Label();
+            this.lblOD = new System.Windows.Forms.Label();
+            this.btnAllOrders = new System.Windows.Forms.Button();
+            this.btnOrderDetails = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtCount = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnPrevious = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.comboBoxYear = new System.Windows.Forms.ComboBox();
+            this.nwDataSet1 = new LinqLabs.NWDataSet();
+            this.productsTableAdapter1 = new LinqLabs.NWDataSetTableAdapters.ProductsTableAdapter();
+            this.ordersTableAdapter1 = new LinqLabs.NWDataSetTableAdapters.OrdersTableAdapter();
+            this.order_DetailsTableAdapter1 = new LinqLabs.NWDataSetTableAdapters.Order_DetailsTableAdapter();
+            this.dataGridViewOrders = new System.Windows.Forms.DataGridView();
+            this.dataGridViewOD = new System.Windows.Forms.DataGridView();
+            this.lblPages = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nwDataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewOrders)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewOD)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.comboBoxYear);
+            this.splitContainer1.Panel1.Controls.Add(this.label4);
+            this.splitContainer1.Panel1.Controls.Add(this.lblPages);
+            this.splitContainer1.Panel1.Controls.Add(this.label6);
+            this.splitContainer1.Panel1.Controls.Add(this.label3);
+            this.splitContainer1.Panel1.Controls.Add(this.txtCount);
+            this.splitContainer1.Panel1.Controls.Add(this.btnOrderDetails);
+            this.splitContainer1.Panel1.Controls.Add(this.btnNext);
+            this.splitContainer1.Panel1.Controls.Add(this.btnPrevious);
+            this.splitContainer1.Panel1.Controls.Add(this.btnAllOrders);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(1262, 673);
+            this.splitContainer1.SplitterDistance = 242;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.tableLayoutPanel1);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.tableLayoutPanel2);
+            this.splitContainer2.Size = new System.Drawing.Size(1262, 427);
+            this.splitContainer2.SplitterDistance = 541;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.OutsetDouble;
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.lblOrders, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.dataGridViewOrders, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 369F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(539, 425);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.OutsetDouble;
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel2.Controls.Add(this.lblOD, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.dataGridViewOD, 0, 1);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.61137F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 88.38863F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(715, 425);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // lblOrders
+            // 
+            this.lblOrders.AutoSize = true;
+            this.lblOrders.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblOrders.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblOrders.Font = new System.Drawing.Font("微軟正黑體", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lblOrders.Location = new System.Drawing.Point(6, 3);
+            this.lblOrders.Name = "lblOrders";
+            this.lblOrders.Size = new System.Drawing.Size(527, 47);
+            this.lblOrders.TabIndex = 0;
+            this.lblOrders.Text = "訂單";
+            // 
+            // lblOD
+            // 
+            this.lblOD.AutoSize = true;
+            this.lblOD.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblOD.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblOD.Font = new System.Drawing.Font("微軟正黑體", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lblOD.Location = new System.Drawing.Point(6, 3);
+            this.lblOD.Name = "lblOD";
+            this.lblOD.Size = new System.Drawing.Size(703, 48);
+            this.lblOD.TabIndex = 0;
+            this.lblOD.Text = "訂單明細";
+            // 
+            // btnAllOrders
+            // 
+            this.btnAllOrders.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.btnAllOrders.Location = new System.Drawing.Point(85, 80);
+            this.btnAllOrders.Name = "btnAllOrders";
+            this.btnAllOrders.Size = new System.Drawing.Size(177, 45);
+            this.btnAllOrders.TabIndex = 0;
+            this.btnAllOrders.Text = "全部訂單";
+            this.btnAllOrders.UseVisualStyleBackColor = false;
+            this.btnAllOrders.Click += new System.EventHandler(this.btnAllOrders_Click);
+            // 
+            // btnOrderDetails
+            // 
+            this.btnOrderDetails.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.btnOrderDetails.Location = new System.Drawing.Point(282, 80);
+            this.btnOrderDetails.Name = "btnOrderDetails";
+            this.btnOrderDetails.Size = new System.Drawing.Size(177, 45);
+            this.btnOrderDetails.TabIndex = 0;
+            this.btnOrderDetails.Text = "某年訂單 / 訂單明細";
+            this.btnOrderDetails.UseVisualStyleBackColor = false;
+            this.btnOrderDetails.Click += new System.EventHandler(this.btnOrderDetails_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(465, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 22);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "年 :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label4.Location = new System.Drawing.Point(81, 44);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(276, 25);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "LINQ - NWDataSet - Orders";
+            // 
+            // txtCount
+            // 
+            this.txtCount.Location = new System.Drawing.Point(86, 178);
+            this.txtCount.Name = "txtCount";
+            this.txtCount.Size = new System.Drawing.Size(100, 30);
+            this.txtCount.TabIndex = 1;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(82, 149);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(78, 22);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "一頁幾筆";
+            // 
+            // btnPrevious
+            // 
+            this.btnPrevious.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.btnPrevious.Location = new System.Drawing.Point(192, 163);
+            this.btnPrevious.Name = "btnPrevious";
+            this.btnPrevious.Size = new System.Drawing.Size(89, 45);
+            this.btnPrevious.TabIndex = 0;
+            this.btnPrevious.Text = "上一頁";
+            this.btnPrevious.UseVisualStyleBackColor = false;
+            this.btnPrevious.Click += new System.EventHandler(this.btnPrevious_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.btnNext.Location = new System.Drawing.Point(501, 163);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(89, 45);
+            this.btnNext.TabIndex = 0;
+            this.btnNext.Text = "下一頁";
+            this.btnNext.UseVisualStyleBackColor = false;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // comboBoxYear
+            // 
+            this.comboBoxYear.FormattingEnabled = true;
+            this.comboBoxYear.Location = new System.Drawing.Point(469, 95);
+            this.comboBoxYear.Name = "comboBoxYear";
+            this.comboBoxYear.Size = new System.Drawing.Size(121, 30);
+            this.comboBoxYear.TabIndex = 3;
+            // 
+            // nwDataSet1
+            // 
+            this.nwDataSet1.DataSetName = "NWDataSet";
+            this.nwDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // productsTableAdapter1
+            // 
+            this.productsTableAdapter1.ClearBeforeFill = true;
+            // 
+            // ordersTableAdapter1
+            // 
+            this.ordersTableAdapter1.ClearBeforeFill = true;
+            // 
+            // order_DetailsTableAdapter1
+            // 
+            this.order_DetailsTableAdapter1.ClearBeforeFill = true;
+            // 
+            // dataGridViewOrders
+            // 
+            this.dataGridViewOrders.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewOrders.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridViewOrders.Location = new System.Drawing.Point(6, 56);
+            this.dataGridViewOrders.Name = "dataGridViewOrders";
+            this.dataGridViewOrders.RowHeadersWidth = 51;
+            this.dataGridViewOrders.RowTemplate.Height = 27;
+            this.dataGridViewOrders.Size = new System.Drawing.Size(527, 363);
+            this.dataGridViewOrders.TabIndex = 1;
+            this.dataGridViewOrders.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewOrders_CellContentClick);
+            // 
+            // dataGridViewOD
+            // 
+            this.dataGridViewOD.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewOD.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridViewOD.Location = new System.Drawing.Point(6, 57);
+            this.dataGridViewOD.Name = "dataGridViewOD";
+            this.dataGridViewOD.RowHeadersWidth = 51;
+            this.dataGridViewOD.RowTemplate.Height = 27;
+            this.dataGridViewOD.Size = new System.Drawing.Size(703, 362);
+            this.dataGridViewOD.TabIndex = 1;
+            // 
+            // lblPages
+            // 
+            this.lblPages.AutoSize = true;
+            this.lblPages.Location = new System.Drawing.Point(362, 181);
+            this.lblPages.Name = "lblPages";
+            this.lblPages.Size = new System.Drawing.Size(69, 22);
+            this.lblPages.TabIndex = 2;
+            this.lblPages.Text = "第 幾 頁";
+            // 
+            // FrmHW2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1262, 673);
+            this.Controls.Add(this.splitContainer1);
+            this.Font = new System.Drawing.Font("微軟正黑體", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "FrmHW2";
+            this.Text = "FrmHW2";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nwDataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewOrders)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewOD)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCount;
+        private System.Windows.Forms.Button btnOrderDetails;
+        private System.Windows.Forms.Button btnAllOrders;
+        private System.Windows.Forms.Label lblOrders;
+        private System.Windows.Forms.Label lblOD;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnPrevious;
+        private System.Windows.Forms.ComboBox comboBoxYear;
+        private NWDataSet nwDataSet1;
+        private NWDataSetTableAdapters.ProductsTableAdapter productsTableAdapter1;
+        private NWDataSetTableAdapters.OrdersTableAdapter ordersTableAdapter1;
+        private NWDataSetTableAdapters.Order_DetailsTableAdapter order_DetailsTableAdapter1;
+        private System.Windows.Forms.DataGridView dataGridViewOrders;
+        private System.Windows.Forms.DataGridView dataGridViewOD;
+        private System.Windows.Forms.Label lblPages;
+    }
+}
---
+++ LinqLabs/HW/FrmHW2.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LinqLabs.HW
+{
+    public partial class FrmHW2 : Form
+    {
+        public FrmHW2()
+        {
+            InitializeComponent();
+            ordersTableAdapter1.Fill(nwDataSet1.Orders);
+            order_DetailsTableAdapter1.Fill(nwDataSet1.Order_Details);
+            productsTableAdapter1.Fill(nwDataSet1.Products);
+
+            List<int> q = (from p in nwDataSet1.Orders
+                           where !p.IsOrderDateNull()
+                           select p.OrderDate.Year).Distinct().ToList();
+
+            this.comboBoxYear.DataSource = q;
+
+        }
+
+        private IEnumerable<NWDataSet.OrdersRow> _ordersAll;
+        private IEnumerable<NWDataSet.OrdersRow> _orders;
+
+        private int _pages;
+        private int _count;
+
+
+        public void ShowlabelResult()
+        {
+            this.lblOrders.Text = $"訂單 共{_ordersAll.Count()}筆";
+            this.lblOD.Text = $"訂單 共{dataGridViewOD.RowCount}筆";
+        }
+        public void SetRows()
+        {
+            if (string.IsNullOrWhiteSpace(txtCount.Text))
+            {
+                _count = 0;
+                return;
+            }
+
+            if (!int.TryParse(txtCount.Text, out _count) || _count <= 0)
+            {
+                _count = 0;
+            }
+        }
+        public void ShowPages()
+        {
+            int totalpages = (int)Math.Ceiling((double)_ordersAll.Count() / _count);
+            
+            if (_count <= 0)
+            {
+                _orders = _ordersAll;
+                lblPages.Text = $"第 {_pages + 1} / {_pages + 1} 頁";
+            }
+            else
+            {
+                _orders = _ordersAll.Skip(_count * _pages).Take(_count);
+
+                lblPages.Text = $"第 {_pages + 1} / {totalpages} 頁";
+            }
+
+            dataGridViewOrders.DataSource = _orders.ToList();
+            ShowlabelResult();
+            
+        }
+
+        private void btnAllOrders_Click(object sender, EventArgs e)
+        {
+            _ordersAll = nwDataSet1.Orders;
+            SetRows();
+            _pages = 0;
+            ShowPages();
+        }
+        private void btnOrderDetails_Click(object sender, EventArgs e)
+        {
+            int year = (int)comboBoxYear.SelectedValue;
+
+            _ordersAll = nwDataSet1.Orders.Where(p => p.OrderDate.Year == year);
+            SetRows();
+            _pages = 0;
+            ShowPages();
+        }
+
+        private void dataGridViewOrders_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int orderID = (int)this.dataGridViewOrders.CurrentRow.Cells[0].Value;
+
+            IEnumerable<NWDataSet.Order_DetailsRow> q = nwDataSet1.Order_Details.Where(p => p.OrderID == orderID);
+
+            dataGridViewOD.DataSource = q.ToList();
+            ShowlabelResult();
+        }
+
+        private void btnPrevious_Click(object sender, EventArgs e)
+        {
+            if (_pages > 0)
+            {
+                _pages--;
+                ShowPages();
+            }
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            if (_count <= 0) return;
+
+            int totalPages = (int)Math.Ceiling((double)_ordersAll.Count() / _count);
+            if (_pages < totalPages - 1)
+            {
+                _pages++;
+                ShowPages();
+            }
+        }
+    }
+}
