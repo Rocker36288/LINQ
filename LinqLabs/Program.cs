f6b29a490d72728404623ed7a17cbc5c587904a9
仁鈞 林 <tommylin19990127@gmail.com>
2025-10-19 19:16:33 +0800

    1019

--- LinqLabs/Program.cs
+++ LinqLabs/Program.cs
@@ -31,9 +31,9 @@ namespace Starter
             //Application.Run(new FrmLINQ_To_XXX());
             //Application.Run(new FrmLinq_To_Entity());
 
-            Application.Run(new FrmHW1());
-            Application.Run(new FrmHW2());
-            Application.Run(new FrmHW3());
+            //Application.Run(new FrmHW1());
+            //Application.Run(new FrmHW2());
+            //Application.Run(new FrmHW3());
             Application.Run(new FrmHW4());
 
         }
